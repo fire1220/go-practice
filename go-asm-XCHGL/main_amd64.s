632bbbccf40f5843560aa15a1e4aee84720e0f12
zhoujiandong <fire1220@live.cn>
2023-11-03 13:57:08 +0800

    XCHGL指令

--- go-asm-XCHGL/main_amd64.s
+++ go-asm-XCHGL/main_amd64.s
@@ -1,7 +1,7 @@
 
 #include "textflag.h"
 
-TEXT ·Xchg(SB), NOSPLIT, $0-20
+TEXT ·Xchg(SB), NOSPLIT, $0-24
     MOVQ	ptr+0(FP), BX   // 注释：接收第一个参数（ptr）放到BX寄存器里
     MOVL	new+8(FP), AX   // 注释：把第二个参数（new）放到寄存器AX里
     XCHGL	AX, 0(BX)       // 注释：原子操作，把0(BX)值和AX值交换
