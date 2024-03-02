31939188706190e6860755591eaac7c2f2e27570
zhoujiandong <fire1220@live.cn>
2024-03-02 10:14:03 +0800

    gRPC

---
+++ go-grpc/client/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"log"
+	pb "main/proto/model"
+	"time"
+)
+
+func main() {
+	// 远程连接凭证，insecure模式下禁用了传输安全认证
+	credentials := grpc.WithTransportCredentials(insecure.NewCredentials())
+	conn, err := grpc.Dial("127.0.0.1:9888", credentials)
+	if err != nil {
+		log.Printf("连接失败：%v\n", err)
+	}
+	defer conn.Close()
+	// 初始化UserService客户端
+	userClient := pb.NewUserServiceClient(conn)
+	// 定义超时上下文
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	// 远程调用服务端方法
+	response, err := userClient.GetUserInfo(ctx, &pb.UserRequest{Name: "fire"})
+	if err != nil {
+		log.Printf("调用GetUserInfo失败:%v\n", err)
+		return
+	}
+	log.Printf("%#v\n", response)
+}
---
+++ go-grpc/server/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"log"
+	pb "main/proto/model"
+	"net"
+)
+
+type UserService struct {
+	pb.UnimplementedUserServiceServer
+}
+
+func (s *UserService) GetUserInfo(ctx context.Context, r *pb.UserRequest) (*pb.UserResponse, error) {
+	log.Printf("try to get user info by %v\n", r.GetName())
+	if len(r.GetName()) == 0 {
+		return nil, errors.New("the query name cannot be empty")
+	}
+	return &pb.UserResponse{Id: 123, Username: "jock", Nickname: r.GetName()}, nil
+}
+
+func main() {
+	// 监听端口
+	ln, err := net.Listen("tcp", "127.0.0.1:9888")
+	if err != nil {
+		log.Printf("listen err: %v\n", err)
+		return
+	}
+	// 实例化gRPC服务
+	server := grpc.NewServer()
+	// 注册UserService服务
+	pb.RegisterUserServiceServer(server, new(UserService))
+	// 向gRPC服务端注册反射服务
+	reflection.Register(server)
+	// 启动gRPC服务
+	if err := server.Serve(ln); err != nil {
+		log.Printf("启动gRPC服务失败:%v\n", err)
+	}
+}
