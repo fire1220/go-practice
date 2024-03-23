afaed90d1255ecb6ece91226008673cdc9814497
zhoujiandong <fire1220@live.cn>
2024-03-23 10:06:29 +0800

    asm-string

--- go-asm-string/main.go
+++ go-asm-string/main.go
@@ -1,25 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"go-string/pkg"
 )
 
 func main() {
-	println(pkg.Name)
-	println(pkg.Title)
-	// str := pkg.Name
-	// t := (*reflect.SliceHeader)(unsafe.Pointer(&str))
-	// var ret []byte
-	// sTemp := (*reflect.SliceHeader)(unsafe.Pointer(&ret))
-	// sTemp.Len = t.Len
-	// sTemp.Cap = t.Len
-	// sTemp.Data = t.Data
-	// fmt.Println(ret)
-	// ret[0] = 111
-	// fmt.Println(ret)
-
-	// str := pkg.Name
-	// ret := []byte(str)
-	// ret[0] = 111
-	// fmt.Println(ret)
+	fmt.Printf("string pgk.Name:%#v\n", pkg.Name)
+	fmt.Printf("string pgk.Name:%#v\n", pkg.Title)
 }
