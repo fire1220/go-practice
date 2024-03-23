31e88ac3c8de4a01d4f4b86e5e70aeb188172262
zhoujiandong <fire1220@live.cn>
2024-03-23 09:34:42 +0800

    asm-string

--- go-asm-string/pkg/pkg_amd64.s
+++ go-asm-string/pkg/pkg_amd64.s
@@ -1,42 +1,44 @@
 // pkg/pkg_amd64.s
 #include "textflag.h"
 
+// NOPTR : 报告函之战
+// RODATA : 只读数据
 // 此方法的gopher不是私有的变量，理论上可以尾部访问
-GLOBL ·Name(SB), NOPTR | RODATA, $24
+GLOBL ·Name(SB), NOPTR | RODATA, $16
 DATA ·Name+0(SB)/8, $·Name+16(SB)
-DATA ·Name+8(SB)/8, $6
-DATA ·Name+16(SB)/8, $"gopher"
+DATA ·Name+8(SB)/8, $11
+DATA ·Name+16(SB)/16, $"hello world"
 
 //  内存分布
-// **********
-// * Name变量*
-// **********
-// *  data  *   0X123
-// **********
-// *  len   *   8
-// **********
-// * gopher *   0X123
-// **********
+// ***************
+// *   Name变量   *
+// ***************
+// *     data    *   0X123  <- ·Name+16(SB)的地址
+// ***************
+// *     len     *   8
+// ***************
+// * hello world *   0X123  <- ·Name+16(SB)的地址
+// ***************
 
-// 设置变量Title
-// 局部变量不用中点（·）
-GLOBL sharedText<>(SB),NOPTR,$8      // 定义局部变量
-DATA sharedText<>+0(SB)/8,$"Golang"  // 定义局部变量的值
-
-// 此方法的Golang字符串时局部变量，不能尾部访问
-GLOBL ·Title(SB),NOPTR,$16
-DATA ·Title+0(SB)/8, $sharedText<>(SB)   // 设置字符串指针指向局部变量
-DATA ·Title+8(SB)/8, $8                  // 设置字符串长度
+// 定义局部变量，变量区分大小写，（局部变量关键词是左右尖括号<>）
+GLOBL title<>(SB), NOPTR | RODATA , $16      // 定义局部变量
+DATA title<>+0(SB)/16, $"hello Golang"       // 定义局部变量的值
+// 定义全局变量（全局变量关键词是重点·）
+GLOBL ·Title(SB), NOPTR | RODATA, $16
+DATA ·Title+0(SB)/8, $title<>(SB)           // 设置字符串指针指向局部变量
+DATA ·Title+8(SB)/8, $16                    // 设置字符串长度
 
 //  内存分布
-// **********
-// * Golang *   0X123
-// **********
-// *  ...   *
-// **********
-// * Name变量*
-// **********
-// *  data  *   0X123
-// **********
-// *  len   *   8
-// **********
+// ****************
+// * Title 全局变量 *
+// ****************
+// *     data     *   0X123   <- &title
+// ****************
+// *     len      *   16
+// ****************
+// *     ...      *
+// ****************
+// * title 局部变量 *   0X123   <-  &title
+// ****************
+// * hello Golang *
+// ****************
