e69c8ea17ccf9830cd2278a4fbcd67521a0847e1
zhoujiandong <fire1220@live.cn>
2024-06-26 18:33:31 +0800

    表格

--- go-excel/simple/main.go
+++ go-excel/simple/main.go
@@ -4,25 +4,40 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/xuri/excelize/v2"
-	"log"
 	"unsafe"
 )
 
 func main() {
-	openFile, err := excelize.OpenFile("file.xlsx") // 读取文件
+	j, err := readToJson("file.xlsx")
 	if err != nil {
-		log.Printf("OpenFile err:%v\n", err)
-		return
+		fmt.Println(err)
+	} else {
+		fmt.Println(j)
 	}
-	excelRowList, err := openFile.GetRows("Sheet1") // 读取行
+}
+
+// 读取表格,返回json
+func readToJson(fileName string, sheetL ...string) (string, error) {
+	sheet := "Sheet1"
+	if len(sheetL) > 0 {
+		sheet = sheetL[0]
+	}
+	openFile, err := excelize.OpenFile(fileName) // 读取文件
 	if err != nil {
-		log.Printf("GetRows err:%v\n", err)
-		return
+		return "", fmt.Errorf("OpenFile err:%v\n", err)
+	}
+	excelRowList, err := openFile.GetRows(sheet) // 读取行
+	if err != nil {
+		return "", fmt.Errorf("GetRows err:%v\n", err)
 	}
 	j, err := json.Marshal(excelRowList)
 	if err != nil {
-		log.Printf("Marshal err:%v\n", err)
-		return
+		return "", fmt.Errorf("Marshal err:%v\n", err)
 	}
-	fmt.Println(*(*string)(unsafe.Pointer(&j)))
+	return *(*string)(unsafe.Pointer(&j)), nil
+}
+
+// 写入表格
+func write() {
+
 }
