7d6bafe4236620e15a157ff45517738dc2afad82
zhoujiandong <fire1220@live.cn>
2024-10-30 18:03:22 +0800

    JWT

--- go-JWT/hmac/main.go
+++ go-JWT/hmac/main.go
@@ -1,39 +1,44 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"github.com/golang-jwt/jwt/v5"
-	"time"
 )
 
 // 对称加密算法的JWT
 func main() {
-	// 创建JWT
-	hmacSampleSecret := []byte("abcd")
-	token1 := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+	m := map[string]any{
 		"foo":  "bar",
-		"nbf":  time.Date(2015, 10, 10, 12, 0, 0, 0, time.UTC).Unix(),
 		"name": "jock",
-	})
-	tokenString1, err := token1.SignedString(hmacSampleSecret)
-	fmt.Println(tokenString1, err)
+	}
+	sig, err := HMACSign([]byte("abcd"), m)
+	fmt.Println("对称加密的JWT")
+	fmt.Println("生成JWT：")
+	// eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmb28iOiJiYXIiLCJuYW1lIjoiam9jayJ9.LbcLniyRlKNOMvWuIYi_GsjsJkKvPWZe8i4CdqnWGrc
+	fmt.Println(sig, err)
+	fmt.Println("验证JWT：")
+	fmt.Println(HMACVerify([]byte("abcd"), sig))
+}
 
-	// 验证JWT
-	tokenString := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmb28iOiJiYXIiLCJuYW1lIjoiam9jayIsIm5iZiI6MTQ0NDQ3ODQwMH0.nlfyvduuBOLVWlsAeYL6g1YCj7VnqJQFxOhknb1h-_0"
+// HMACSign 创建JWT
+func HMACSign(hmacSecret []byte, m map[string]any) (string, error) {
+	return jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims(m)).SignedString(hmacSecret)
+}
+
+// HMACVerify 验证JWT
+func HMACVerify(hmacSecret []byte, tokenString string) (jwt.MapClaims, error) {
 	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Method.Alg())
 		}
-		return hmacSampleSecret, nil
+		return hmacSecret, nil
 	})
 	if err != nil {
-		fmt.Println(err)
-		return
+		return nil, err
 	}
 	if !token.Valid {
-		fmt.Println("Invalid token")
-		return
+		return nil, errors.New("invalid token")
 	}
-	// fmt.Printf("%#v\n", token)
-	fmt.Println(token.Claims.(jwt.MapClaims))
+	return token.Claims.(jwt.MapClaims), nil
 }
