2e183c76fb07e052b40b7f0f0c5014572af6107d
zhoujiandong <fire1220@live.cn>
2024-11-19 14:34:47 +0800

    marshalijson

--- go-json-marshal/marshaljson/marshal.go
+++ go-json-marshal/marshaljson/marshal.go
@@ -7,33 +7,6 @@ import (
 	"time"
 )
 
-const (
-	tabDefault  = "default"
-	tabDateTime = "datetime"
-)
-
-type tabT struct {
-	refTypOf reflect.Type
-	restrain string
-	fun      typeConvT
-}
-
-var (
-	tabList = []string{tabDefault, tabDateTime}
-	tabMap  = map[string]tabT{
-		tabDefault: {
-			refTypOf: reflect.TypeOf(defaultT{}),
-			restrain: "",
-			fun:      defaultT{},
-		},
-		tabDateTime: {
-			refTypOf: reflect.TypeOf(dateTime{}),
-			restrain: "time.Time",
-			fun:      dateTime{},
-		},
-	}
-)
-
 func MarshalFormat(p any) ([]byte, error) {
 	ref := reflect.ValueOf(p)
 	typ := ref.Type()
